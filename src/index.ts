ad89a4bb0050c4627bb1b1e59b32182226b4bf0a
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-02-13 14:22:27 +0100

    node-server installed.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { getClient } from "./db/db";
-import { serve } from "bun";
+import { serve } from "@hono/node-server";
 import { subscriberRouter } from "./routes/subscriber";
 import { newsletterRouter } from "./routes/newsletter";
 
